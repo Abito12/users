8f776dbdc66bcf6d5a1577425e4d72c77652d7d1
Abito Prakash <Abito.Prakash@amdocs.com>
2021-07-19 16:34:36 +0530

    Minor fix in add user

--- src/api/User/UserApi.js
+++ src/api/User/UserApi.js
@@ -6,7 +6,7 @@ class UserApi {
     }
 
     static addUser(details) {
-        const users = JSON.parse(localStorage.getItem('users') || []);
+        const users = JSON.parse(localStorage.getItem('users'));
         localStorage.setItem('users', JSON.stringify([...users, {...details, isAdmin: false}]));
     }
 }
